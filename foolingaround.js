011ba01a8a32777b1ebda5bfedb925ed956004de
lorleod <taylor.mcleod@gmail.com>
2022-03-17 22:22:06 +0000

    more practice problems and learnign

--- foolingaround.js
+++ foolingaround.js
@@ -1,36 +1,23 @@
-function calculateRectangleArea (length, width) {
-  let area;
-  if (length < 0 || width < 0) {
-    return area;
+function range (start, end, step) {
+  if (start == undefined || end == undefined || step == undefined) {
+  //returns empty array if any parameter is undefined
+    return [];
   }
-  return length * width;
+
+  if (start > end || step < 1) {
+    //returns empty array if start > end or step < 1
+    return [];
+  }
+
+  let outputArray = [];
+  let value = start;
+  
+  while (value < end) {
+    outputArray.push(value);
+    value += step;
+  }
+  
+  return outputArray;
 }
 
-function calculateTriangleArea (base, height) {
-  let area;
-  if (base < 0 || height < 0) {
-    return area;
-  }
-  return base * height / 2;
-}
-
-function calculateCircleArea (radius) {
-  let area;
-  if (radius < 0) {
-    return area;
-  }
-  return Math.PI * radius * radius;
-}
-
-
-console.log(calculateRectangleArea(10, 5));     // should print 50
-console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
-console.log(calculateRectangleArea(10, -5));    // should print undefined
-
-console.log(calculateTriangleArea(10, 5)); // should print 25
-console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
-console.log(calculateTriangleArea(10, -5)); // should print undefined
-
-console.log(calculateCircleArea(10)); // should print 314.159...
-console.log(calculateCircleArea(3.5)); // should print 38.484...
-console.log(calculateCircleArea(-1)); // should print undefined
+console.log(range(0, 34, 5));
