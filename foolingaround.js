f889af3cc2a39d3229b2001feccc73ce3d621707
lorleod <taylor.mcleod@gmail.com>
2022-04-15 23:26:39 +0000

    kata 5

--- foolingaround.js
+++ foolingaround.js
@@ -1,25 +1,16 @@
-const instructorWithLongestName = function(instructors) {
-  let count = 0;
-  let longest = [];
-
-  for (let i = 0; i < instructors.length; i++) {
-    if (instructors[i].name.length > count) {
-      count = instructors[i].name.length;
-      longest = instructors[i];
+const urlEncode = function(text) {
+  let trimmedText = text.trim();
+  let encodedText = "";
+  for (let i = 0; i < trimmedText.length; i++) {
+    if (trimmedText[i] != " ") {
+      encodedText = encodedText.concat(trimmedText[i]);
+    } else if (trimmedText[i] === " ") {
+      encodedText = encodedText.concat("%20");
     }
   }
+return encodedText;
+};
 
-  return longest;
-}
-
-console.log(instructorWithLongestName([
-  {name: "Samuel", course: "iOS"},
-  {name: "Jeremiah", course: "Web"},
-  {name: "Ophilia", course: "Web"},
-  {name: "Donald", course: "Web"}
-]));
-console.log(instructorWithLongestName([
-  {name: "Matthew", course: "Web"},
-  {name: "David", course: "iOS"},
-  {name: "Domascus", course: "Web"}
-]));
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
